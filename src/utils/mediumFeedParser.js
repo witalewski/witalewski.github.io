dec2d2ce9046d18d41fa5cc85d16027c1071952c
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-13 17:19:27 +0100

    rewrite medium feed parser with functions

--- src/utils/mediumFeedParser.js
+++ src/utils/mediumFeedParser.js
@@ -1,29 +1,56 @@
-import xmlJS from 'xml-js';
-export const parseMediumFeed = data => {
-  const parser = new DOMParser();
-  const {
-    rss: {
-      channel: { item },
-    },
-  } = JSON.parse(xmlJS.xml2json(data, { compact: true }));
-  const feed = item.map(item => {
-    const document = parser.parseFromString(
-      item['content:encoded']._cdata,
-      'text/html'
-    );
-    const paragraphs = document.getElementsByTagName('p');
-    const contentStrings = [];
-    for (let i = 0; i < paragraphs.length; i++) {
-      contentStrings.push(paragraphs[i].innerText);
-    }
-    return {
-      title: item.title._cdata,
-      link: item.link._text,
-      date: item.pubDate._text,
-      categories: item.category.map(category => category._cdata),
-      image: document.getElementsByTagName('img')[0].src,
-      content: `${contentStrings.join(' ').substr(0, 180).match(/(.*)\W/)[1]}...`,
-    };
-  });
-  return feed;
+import * as R from 'ramda';
+
+const parseXML = data => new DOMParser().parseFromString(data, 'text/html');
+
+const htmlCollectionToArray = htmlCollection => {
+  const array = [];
+  for (let i = 0; i < htmlCollection.length; i++) {
+    array.push(htmlCollection.item(i));
+  }
+  return array;
 };
+
+const getTags = tagName => element =>
+  htmlCollectionToArray(element.getElementsByTagName(tagName));
+const getTag = tagName =>
+  R.compose(
+    R.head,
+    getTags(tagName)
+  );
+
+const itemToItemWithContentEncoded = item => ({
+  item,
+  contentEncoded: getTag('content:encoded')(item),
+});
+
+const removeCdata = str => str.match(/<!\[CDATA\[(.*)\]\]>/)[1];
+
+const getInnerText = R.prop('innerText');
+
+const trim = s => s.substr(0, 160).match(/(.*)\W/)[1].concat('...');
+
+const getSummary = R.compose(
+  trim,
+  R.join(' '),
+  R.map(getInnerText),
+  getTags('p')
+);
+
+const itemWithContentEncodedToResult = ({ item, contentEncoded }) => ({
+  title: removeCdata(getTag('title')(item).innerText),
+  link: getTag('link')(item).innerText,
+  date: getTag('pubDate')(item).innerText,
+  image: getTag('img')(contentEncoded).src,
+  content: getSummary(contentEncoded),
+});
+
+export const parseMediumFeed = R.compose(
+  R.map(
+    R.compose(
+      itemWithContentEncodedToResult,
+      itemToItemWithContentEncoded
+    )
+  ),
+  getTags('item'),
+  parseXML
+);
