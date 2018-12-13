0f1d1c9652372165015c4a2bb6136556084b4fd3
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-13 17:45:12 +0100

    add coveralls

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # witalewski.github.io
 [![Build Status](https://travis-ci.org/witalewski/witalewski.github.io.svg?branch=dev)](https://travis-ci.org/witalewski/witalewski.github.io)
+[![Coverage Status](https://coveralls.io/repos/github/witalewski/witalewski.github.io/badge.svg?branch=dev)](https://coveralls.io/github/witalewski/witalewski.github.io?branch=dev)
 
 Content for http://witalewski.tech
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom';import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { codeReducer } from './reducers/codeReducer';
+import { writingReducer } from './reducers/writingReducer';
+import { photosReducer } from './reducers/photosReducer';
+import { App } from './App';
 
 it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement('div');
+  const reducers = {
+    code: codeReducer,
+    writing: writingReducer,
+    photos: photosReducer,
+  };
+  const store = createStore(
+    combineReducers(reducers),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+  
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
