366295a1f819204c109666bd181ba2a05d2259d2
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-13 15:25:48 +0100

    update storybook

--- stories/Photos.stories.js
+++ stories/Photos.stories.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { storiesOf, addDecorator } from '@storybook/react';
 import { Photos } from '../src/components/Photos/Photos';
+import mockPhotos from '../src/mocks/mockPhotos.json';
 import { decorator } from './decorator';
 
 addDecorator(decorator);
 
-storiesOf('Photos', module).add('loading a list from Instagram', () => <Photos />);
+storiesOf('Photos', module).add('with a predefined list from Instagram', () => <Photos n={15} photos={mockPhotos} />);
--- stories/Writing.stories.js
+++ stories/Writing.stories.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { storiesOf, addDecorator } from '@storybook/react';
 import { Writing } from '../src/components/Writing/Writing';
 import { decorator } from './decorator';
+import mockWriting from '../src/mocks/mockWriting.json'
 
 addDecorator(decorator);
 
-storiesOf('Writing', module).add('loading a list from Medium', () => <Writing />);
+storiesOf('Writing', module).add('with a predefined list from Medium', () => <Writing n={4} m={2} posts={mockWriting} />);
