59dd232625e27ee7180df7638ab24c5a3999efd1
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-14 10:38:26 +0100

    add thunk

--- src/actions.js
+++ src/actions.js
@@ -1,19 +1,30 @@
+import * as R from 'ramda';
 import * as api from './api';
+
+const createFetchAction = (fetchFunction, receiveAction) => () => dispatch =>
+  fetchFunction().then(
+    R.compose(
+      dispatch,
+      receiveAction
+    )
+  );
 
 const receiveCode = response => ({
   type: 'RECEIVE_CODE',
   response,
 });
-export const fetchCode = () => api.getCode().then(receiveCode);
+export const fetchCode = createFetchAction(api.getCode, receiveCode);
+
 
 const receiveWriting = response => ({
   type: 'RECEIVE_WRITING',
   response,
 });
-export const fetchWriting = () => api.getWriting().then(receiveWriting);
+export const fetchWriting = createFetchAction(api.getWriting, receiveWriting);
 
 const receivePhotos = response => ({
   type: 'RECEIVE_PHOTOS',
   response,
 });
-export const fetchPhotos = () => api.getPhotos().then(receivePhotos);
+export const fetchPhotos = createFetchAction(api.getPhotos, receivePhotos);
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import * as R from 'ramda';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { codeReducer } from './reducers/codeReducer';
 import { writingReducer } from './reducers/writingReducer';
 import { photosReducer } from './reducers/photosReducer';
 import * as actions from './actions';
+import { App } from './App';
 
 const reducers = {
   code: codeReducer,
@@ -15,18 +17,15 @@ const reducers = {
 };
 const store = createStore(
   combineReducers(reducers),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  R.compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
-const dispatchWithPromiseSupport = store => {
-  const rawDispatch = store.dispatch;
-  return action =>
-    typeof action.then === 'function'
-      ? action.then(rawDispatch)
-      : rawDispatch(action);
-};
-
-[actions.fetchWriting(), actions.fetchCode(), actions.fetchPhotos()].forEach(dispatchWithPromiseSupport(store));
+[actions.fetchWriting(), actions.fetchCode(), actions.fetchPhotos()].forEach(
+  store.dispatch
+);
 
 ReactDOM.render(
   <Provider store={store}>
