1f181bb2434328ee3c6ebdc30eccf378af0963df
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-19 13:01:08 +0100

    add background to break image

--- src/components/ImageBreak/ImageBreakStyled.js
+++ src/components/ImageBreak/ImageBreakStyled.js
@@ -2,6 +2,11 @@ import styled from '@emotion/styled';
 import { BREAKPOINT } from '../../global/Constants';
 
 export const ImageBreakStyled = styled.div`
+  width: 100%;
+  height: 200px;
+
+  background: #f0f0f0;
+
   .break-image {
     width: 100%;
     height: 200px;
