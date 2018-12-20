e379dcca83e5d3deb2276b18955d0ea05b3f8411
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-20 21:54:18 +0100

    add typescript color for repos

--- src/components/Code/GitRepo.js
+++ src/components/Code/GitRepo.js
@@ -5,6 +5,7 @@ import { getRelativeDate } from '../../utils/relativeDateFormatter';
 const languageColors = {
   JavaScript: '#f1e05a',
   HTML: '#e34c26',
+  TypeScript: '#2b7489',
 };
 
 export const GitRepo = ({ repo }) => (
