5584fbe51545b3f5c664f216a5be47096ae2fd59
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-06-09 22:30:04 +0200

    fix github token

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,31 +7,31 @@
 module.exports = {
   plugins: [
     {
-      resolve: "gatsby-source-medium",
+      resolve: 'gatsby-source-medium',
       options: {
-        username: "@awitalewski",
-        limit: 4
-      }
+        username: '@awitalewski',
+        limit: 4,
+      },
     },
     {
-      resolve: "gatsby-source-github-pinned",
+      resolve: 'gatsby-source-github-pinned',
       options: {
-        apiToken: "9973b2c47a5d580ec4f641616c0ab7e176bcb956"
-      }
+        apiToken: 'aa29261c428906758bf2ce36af650b59b4da3493',
+      },
     },
     {
-      resolve: "gatsby-plugin-web-font-loader",
+      resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ["Lato"]
-        }
-      }
+          families: ['Lato'],
+        },
+      },
     },
     {
-      resolve: "gatsby-source-instagram",
+      resolve: 'gatsby-source-instagram',
       options: {
-        username: "nihilismislove"
-      }
-    }
-  ]
+        username: 'nihilismislove',
+      },
+    },
+  ],
 };
--- public/styles-f8d220d9b66935fa16ae.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{200:function(n,w,o){}}]);
-//# sourceMappingURL=styles-f8d220d9b66935fa16ae.js.map
