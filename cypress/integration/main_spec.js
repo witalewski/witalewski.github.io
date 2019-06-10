9c6b861e4ea0ba1d61f9e2156ee3074507c6219e
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-06-10 20:17:33 +0200

    add additional tests|

--- cypress/integration/main_spec.js
+++ cypress/integration/main_spec.js
@@ -5,6 +5,12 @@ describe('Main test', function() {
     it('finds name', function() {
         cy.contains('Adam Witalewski')
     })
+    it('finds medium articles', function() {
+        cy.get('a[href^="https://medium.com/@awitalewski"]').should('have.length', 4)
+    })
+    it('finds github repos ', function() {
+        cy.get('a[href^="https://github.com/witalewski"]').should('have.length', 5)
+    })
     it('finds instagram feed', function() {
         cy.get('a[href^="https://www.instagram.com/p/"]').should('have.length', 12)
     })
