16103cc61dae95a2459a2b5c0a661190506d5b07
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-06-10 06:32:26 +0200

    fix hotjar script

--- src/components/SiteMetadata.js
+++ src/components/SiteMetadata.js
@@ -29,19 +29,21 @@ const SiteMetadata = () => {
     }
   `);
 
-  (function(h, o, t, j, a, r) {
-    h.hj =
-      h.hj ||
-      function() {
-        (h.hj.q = h.hj.q || []).push(arguments);
-      };
-    h._hjSettings = { hjid: 1355687, hjsv: 6 };
-    a = o.getElementsByTagName('head')[0];
-    r = o.createElement('script');
-    r.async = 1;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-    a.appendChild(r);
-  })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+  if (window) {
+    (function(h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function() {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid: 1355687, hjsv: 6 };
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script');
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+  }
 
   return (
     <Helmet defaultTitle={title} titleTemplate={`%s | ${title}`}>
