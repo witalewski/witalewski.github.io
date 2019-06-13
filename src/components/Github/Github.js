76c31409793fb1afefb0ec109876af3495727aa7
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-06-13 18:06:29 +0200

    refactor components

--- src/components/Github/Github.js
+++ src/components/Github/Github.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import { shape, arrayOf, string } from 'prop-types';
 import {
   GithubStyled,
   ItemStyled,
@@ -8,41 +9,48 @@ import {
   RepoDescriptionStyled,
 } from './GithubStyled';
 
-export const Github = () => (
-  <StaticQuery
-    query={graphql`
-      query PinnedRepos {
-        allPinnedRepo {
-          edges {
-            node {
-              name
-              url
-              description
-              updatedAt(fromNow: true)
-            }
-          }
+const query = graphql`
+  query PinnedRepos {
+    allPinnedRepo {
+      edges {
+        node {
+          name
+          url
+          description
+          updatedAt(fromNow: true)
         }
       }
-    `}
-    render={data => {
-      const { edges } = data.allPinnedRepo;
+    }
+  }
+`;
+
+const render = ({ allPinnedRepo: { edges } }) => (
+  <GithubStyled>
+    {edges.map(({ node }) => {
+      const { name, url, description } = node;
       return (
-        <GithubStyled>
-          {edges.map(({ node }) => {
-            const { name, url, description } = node;
-            return (
-              <ItemStyled key={url}>
-                <LinkStyled href={url} rel="noopener">
-                  <RepoNameStyled>{name}</RepoNameStyled>
-                  <RepoDescriptionStyled>{description}</RepoDescriptionStyled>
-                </LinkStyled>
-              </ItemStyled>
-            );
-          })}
-        </GithubStyled>
+        <ItemStyled key={url}>
+          <LinkStyled href={url} rel="noopener">
+            <RepoNameStyled>{name}</RepoNameStyled>
+            <RepoDescriptionStyled>{description}</RepoDescriptionStyled>
+          </LinkStyled>
+        </ItemStyled>
       );
-    }}
-  />
+    })}
+  </GithubStyled>
 );
+render.propTypes = {
+  allPinnedRepo: shape({
+    edges: arrayOf({
+      node: shape({
+        name: string,
+        url: string,
+        description: string,
+      }),
+    }),
+  }).isRequired,
+};
+
+export const Github = () => <StaticQuery query={query} render={render} />;
 
 export default Github;
--- src/components/Medium/Medium.js
+++ src/components/Medium/Medium.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import { shape, arrayOf, string } from 'prop-types';
 import {
   MediumStyled,
   ItemStyled,
@@ -9,61 +10,75 @@ import {
   DescriptionStyled,
 } from './MediumStyled';
 
-export const Medium = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allMediumPost(sort: { fields: [createdAt], order: DESC }) {
-          edges {
-            node {
-              id
-              title
-              uniqueSlug
-              virtuals {
-                subtitle
-                previewImage {
-                  imageId
-                }
-              }
+const query = graphql`
+  query {
+    allMediumPost(sort: { fields: [createdAt], order: DESC }) {
+      edges {
+        node {
+          id
+          title
+          uniqueSlug
+          virtuals {
+            subtitle
+            previewImage {
+              imageId
             }
           }
         }
       }
-    `}
-    render={data => {
-      const { edges } = data.allMediumPost;
-      return (
-        <MediumStyled>
-          {edges.map(
-            ({
-              node: {
-                title,
-                uniqueSlug,
-                virtuals: {
-                  subtitle,
-                  previewImage: { imageId },
-                },
-              },
-            }) => (
-              <ItemStyled key={title}>
-                <LinkStyled
-                  href={`https://medium.com/@awitalewski/${uniqueSlug}`}
-                  rel="noopener"
-                >
-                  <PostTitleStyled>{title}</PostTitleStyled>
-                  <ThumbnailStyled
-                    src={`https://cdn-images-1.medium.com/max/360/${imageId}`}
-                    alt={`Illustration for blog post: ${title}`}
-                  />
-                  <DescriptionStyled>{subtitle}</DescriptionStyled>
-                </LinkStyled>
-              </ItemStyled>
-            )
-          )}
-        </MediumStyled>
-      );
-    }}
-  />
-);
+    }
+  }
+`;
+
+const render = ({ allMediumPost: { edges } }) => {
+  return (
+    <MediumStyled>
+      {edges.map(
+        ({
+          node: {
+            title,
+            uniqueSlug,
+            virtuals: {
+              subtitle,
+              previewImage: { imageId },
+            },
+          },
+        }) => (
+          <ItemStyled key={title}>
+            <LinkStyled
+              href={`https://medium.com/@awitalewski/${uniqueSlug}`}
+              rel="noopener"
+            >
+              <PostTitleStyled>{title}</PostTitleStyled>
+              <ThumbnailStyled
+                src={`https://cdn-images-1.medium.com/max/360/${imageId}`}
+                alt={`Illustration for blog post: ${title}`}
+              />
+              <DescriptionStyled>{subtitle}</DescriptionStyled>
+            </LinkStyled>
+          </ItemStyled>
+        )
+      )}
+    </MediumStyled>
+  );
+};
+render.propTypes = {
+  allMediumPost: shape({
+    edges: arrayOf(
+      shape({
+        node: shape({
+          title: string,
+          uniqueSlug: string,
+          virtuals: shape({
+            subtitle: string,
+            previewImage: shape({ imageId: string }),
+          }),
+        }),
+      })
+    ),
+  }).isRequired,
+};
+
+export const Medium = () => <StaticQuery query={query} render={render} />;
 
 export default Medium;
