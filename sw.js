10580dcb4f5a0bd346000a4460c1299752717dd3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-27 23:08:10 +0000

    Deploy witalewski/witalewski.github.io to github.com/witalewski/witalewski.github.io.git:master

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a6668fa60fb5ed5b0eef.js"
+    "url": "webpack-runtime-27b2adfac6f6fc96dd62.js"
   },
   {
     "url": "app-ac9a703ec44fe7ab55aa.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "47f3c136fa91c162a8b18303b5a9341e"
+    "revision": "632be6cfa687d815f7e50e71c0fe8a9a"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
