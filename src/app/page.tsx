025c68fac46790a3f4f85386713972c685a2c1b9
Adam Witalewski <witalewski@gmail.com>
2024-01-12 11:33:54 +0100

    Improve layout on the smallest screens; remove unused tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-12">
+    <main className="flex min-h-screen flex-col items-center p-6 sm:p-12">
       <Image
         src="/portrait.jpg"
         alt="Portrait photo of Krzysztof Adam Witalewski steering a yacht"
-        className="rounded-full mb-10 border border-zinc-900 dark:border-zinc-300"
+        className="rounded-full border border-zinc-900 dark:border-zinc-300 mb-3 xs:mb-10"
         width={240}
         height={240}
-        priority
       />
-      <h1 className="text-3xl mb-1">Krzysztof Adam Witalewski</h1>
-      <h2 className="text-xl">Software Engineer</h2>
+      <h1 className="text-3xl mb-2 sm:mb-1 text-center">Krzysztof Adam Witalewski</h1>
+      <h2 className="text-xl text-center">Software Engineer</h2>
     </main>
   );
 }
